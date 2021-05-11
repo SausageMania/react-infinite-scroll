5b88ad19f851c3ea0bdf12720e386fcaaeb17901
SausageMania <joshua5451@naver.com>
2021-05-11 17:49:25 +0900

    scroll에 따른 dynamic text size 변경

--- src/Scrolling.js
+++ src/Scrolling.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 // eslint-disable-next-line no-unused-vars
 import { throttle } from 'lodash';
 import { Box, Typography } from '@material-ui/core';
 
-const Scrolling = () => {
+const Scrolling = props => {
+    const { position, screenHeight } = props;
+    const [textSize, setTextSize] = useState('50px');
+
+    useEffect(() => {
+        if (position - screenHeight / 1.5 < 0) {
+            setTextSize('100px');
+        } else {
+            const dynamicSize = ((position - screenHeight / 1.5) / (screenHeight / 1.5)) * 50 + 50;
+            setTextSize(dynamicSize + 'px');
+        }
+    }, [position, screenHeight]);
+
     return (
         <Box display="flex" alignItems="center">
-            <Typography style={{ fontSize: '50px', fontWeight: 600, color: 'white' }}>
+            <Typography style={{ fontSize: textSize, fontWeight: 600, color: 'white' }}>
                 Parallax Scroll!
             </Typography>
         </Box>
